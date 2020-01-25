0bf7a8ede31d59835f82e32111049daa253930e9
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-01-25 17:59:28 +0100

    SortIt: added api request

--- show_it/src/App.vue
+++ show_it/src/App.vue
@@ -1,25 +1,37 @@
 <template>
     <div id="app">
-        <label for="list">Galaxy: </label><select id="list">
-            <option v-for="galaxy in myJson" v-bind:key="galaxy.abbreviation">
+        <label for="list">Galaxy: </label>
+        <select id="list" v-model="selected" @change="GetJsonData()">
+            <option v-for="galaxy in myJson" v-bind:key="galaxy.abbreviation" v-bind:value="galaxy.abbreviation">
                 {{galaxy.name}}
             </option>
         </select>
+        <span>Selected: {{ selected }}</span>
+        <div>Json:<br>{{JSON.stringify(this.result)}}</div>
     </div>
 </template>
 
 <script>
-    import json from './data/constellations.json'
+    import json from './data/constellations.json';
+    import axios from 'axios';
 
     export default {
         name: 'app',
         components: {},
         data() {
             return {
-                myJson: json
+                myJson: json,
+                selected: 'And',
+                result: 'üres'
+            }
+        },
+        methods: {
+            GetJsonData: function () {
+                let url = 'https://www.astropical.space/api.php?table=stars&which=constellation&limit=' + this.selected + '&format=json';
+                axios.get(url).then(response => (this.result = response.data));
             }
         }
-    }
+    };
 </script>
 
 <style>
