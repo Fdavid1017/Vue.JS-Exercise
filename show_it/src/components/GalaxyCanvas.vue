ebad43d8b9fb465faf9d243b41deeee3015d243b
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-01-26 14:45:36 +0100

    ShowIt:cancas drawing

--- show_it/src/components/GalaxyCanvas.vue
+++ show_it/src/components/GalaxyCanvas.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
         <div>{{data}}</div>
-        <br><br><br>
-        <div>{{test}}</div>
+        <br><br>
         <canvas id='dataCanvas' v-bind:width="this.canvasWidth" v-bind:height="this.canvasHeight"></canvas>
     </div>
 </template>
 
 <script>
+    /* eslint-disable no-console */
+
     export default {
         props: [
             'data'
@@ -17,16 +18,14 @@
         data() {
             return {
                 vueCanvas: null,
-                test: 'start\n',
-                canvasWidth: 500,
-                canvasHeight: 500,
+                canvasWidth: 250,
+                canvasHeight: 250,
             }
         },
 
         mounted() {
             let c = document.getElementById("dataCanvas");
-            let ctx = c.getContext("2d");
-            this.vueCanvas = ctx;
+            this.vueCanvas = c.getContext("2d");
             /*ctx.beginPath();
             ctx.arc(95, 50, 40, 0, 2 * Math.PI);
             ctx.stroke();*/
@@ -46,27 +45,36 @@
             data: function () {
                 this.vueCanvas.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
 
-                /*   let highestRA = this.data.hipstars[0].ra + this.data.hipstars[0].radius;
-                   let highestDE = this.data.hipstars[0].de + this.data.hipstars[0].radius;
-                   for (let i = 1; i < this.data.hipstars.length; i++) {
-                       let raValue = Math.abs(this.data.hipstars[i].ra + this.data.hipstars[i].radius);
-                       let deValue = Math.abs(this.data.hipstars[i].de + this.data.hipstars[i].radius);
-                       if (raValue > highestRA) {
-                           highestRA = raValue;
-                       }
-                       if (deValue > highestDE) {
-                           highestDE = deValue;
-                       }
-                   }
+                let highestRA = this.data.hipstars[0].ra + this.data.hipstars[0].radius;
+                let highestDE = this.data.hipstars[0].de + this.data.hipstars[0].radius;
+                for (let i = 1; i < this.data.hipstars.length; i++) {
+                    let raValue = this.xOrigin + this.data.hipstars[i].ra + this.data.hipstars[i].radius;
+                    let deValue = this.yOrigin + this.data.hipstars[i].de + this.data.hipstars[i].radius;
+                    if (raValue > highestRA) {
+                        highestRA = raValue;
+                    }
+                    if (deValue > highestDE) {
+                        highestDE = deValue;
+                    }
+                    console.log('Highest RA:' + highestRA + ' Highest DE:' + highestDE);
+                    console.log('RA:' + raValue + '  DE:' + deValue);
+                }
 
-                   this.canvasWidth = highestRA;
-                   this.canvasHeight = highestDE;*/
+                this.canvasWidth = highestRA;
+                this.canvasHeight = highestDE;
+
+                //  console.log('Width: '+this.canvasWidth+' Height: '+this.canvasHeight);
 
                 for (let i = 0; i < this.data.hipstars.length; i++) {
                     let current = this.data.hipstars[i];
                     this.vueCanvas.beginPath();
                     this.vueCanvas.arc(this.xOrigin + current.ra, this.yOrigin + current.de, current.radius, 0, 2 * Math.PI);
                     this.vueCanvas.stroke();
+
+                    if (this.xOrigin + current.ra > this.canvasWidth || this.yOrigin + current.de > this.canvasHeight) {
+                        console.error('X: ' + (this.xOrigin + current.ra) + '   Y: ' + (this.yOrigin + current.de) + '   Radius: ' + current.radius)
+                    }
+                    //console.log('X: ' + (this.xOrigin + current.ra) + '   Y: ' + (this.yOrigin + current.de) + '   Radius: ' + current.radius)
                 }
             }
         }
