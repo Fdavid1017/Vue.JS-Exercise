9fcae6c8926a0d4d2f1105d131713d94765f76e4
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-01-27 11:42:13 +0100

    ShowIt: canvas drawing

--- show_it/src/components/GalaxyCanvas.vue
+++ show_it/src/components/GalaxyCanvas.vue
@@ -6,21 +6,6 @@
 
 <script>
     /* eslint-disable no-console */
-
-    // eslint-disable-next-line no-unused-vars
-    function getCoordinates(ra, x) {
-        let hour, min, sec, temp;
-        hour = parseInt(ra);
-        temp = (ra - hour) * 60;
-        min = parseInt(temp);
-        temp -= min;
-        temp *= 60;
-        sec = parseInt(temp);
-        x = hour + ':' + min + ':' + sec;
-        // console.log(hour + ':' + min + ':' + sec);
-         console.log(x);
-    }
-
     export default {
         props: [
             'data'
@@ -30,8 +15,8 @@
         data() {
             return {
                 vueCanvas: null,
-                canvasWidth: 181,
-                canvasHeight: 181,
+                canvasWidth: 500,
+                canvasHeight: 500,
             }
         },
 
@@ -58,57 +43,37 @@
                 //clear the canvas
                 this.vueCanvas.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
 
-                //resizing the canvas
-                /*let highestRA = this.data.hipstars[0].ra + this.data.hipstars[0].radius;
-                let highestDE = this.data.hipstars[0].de + this.data.hipstars[0].radius;
-                for (let i = 1; i < this.data.hipstars.length; i++) {
-                    let raValue =Math.abs(this.xOrigin + this.data.hipstars[i].ra + this.data.hipstars[i].radius);
-                    let deValue = Math.abs(this.yOrigin + this.data.hipstars[i].de + this.data.hipstars[i].radius);
-                    if (raValue > highestRA) {
-                        highestRA = raValue;
-                    }
-                    if (deValue > highestDE) {
-                        highestDE = deValue;
-                    }
-                }
-
-                this.canvasWidth = highestDE;
-                this.canvasHeight = highestRA;
-
-                console.log('Width: ' + this.canvasWidth + ' Height: ' + this.canvasHeight);*/
-
-                //drawing the stars to the canvas
-                /*for (let i = 0; i < this.data.hipstars.length; i++) {
+                for (let i = 0; i < this.data.hipstars.length; i++) {
                     let current = this.data.hipstars[i];
+
+                    //converting celestial coordinates to cartesian coordinates
+                    let hours, minutes, seconds, temp;
+                    hours = parseInt(current.ra);
+                    temp = (current.ra - hours) * 60;
+                    minutes = parseInt(temp);
+                    temp -= minutes;
+                    temp *= 60;
+                    seconds = parseInt(temp);
+
+                    let a = (hours * 15) + (minutes * 0.25) + (seconds * 0.004166);
+                    let b = current.de; //current.de
+                    let c = current.dist;//current.dist
+                    let x, y;
+                    /*
+                    X = (C * cos(B)) * cos(A)
+                    Y = (C * cos(B)) * sin(A)
+                    Z = C * sin(B)
+                    */
+                    x = (c * Math.cos(b)) * Math.cos(a);
+                    y = (c * Math.cos(b)) * Math.sin(a);
+
+                    //Drawing stars
                     this.vueCanvas.beginPath();
-                    this.vueCanvas.arc(current.ra,current.de, current.mag, 0, 2 * Math.PI);
+                    this.vueCanvas.arc(this.xOrigin + x, this.yOrigin + y, current.mag, 0, 2 * Math.PI);
                     this.vueCanvas.stroke();
-
-                    if (this.xOrigin + current.ra > this.canvasWidth || this.yOrigin + current.mag > this.canvasHeight) {
-                        console.error('X: ' + (this.xOrigin + current.ra) + '   Y: ' + (this.yOrigin + current.de) + '   Radius: ' + current.mag)
-                    } else {
-                        console.log('X: ' + (this.xOrigin + current.ra) + '   Y: ' + (this.yOrigin + current.de) + '   Radius: ' + current.mag)
-                    }
-                }*/
-
-                //this.getCoordinates(1.734760,0,0,0)
-                let x = "asd";
-                getCoordinates(1.734760, x);
-                console.log(x);
+                }
             }
-        },
-        /* // eslint-disable-next-line no-unused-vars
-         getCoordinates: function (ra, de, x, y) {
-             let hour, min, sec, temp;
-             hour = parseInt(ra);
-             temp = (ra - hour) * 60;
-             min = parseInt(temp);
-             temp -= min;
-             temp *= 60;
-             sec = parseInt(temp);
-
-             console.log(hour + ':' + min + ':' + sec);
-         }*/
+        }
     }
 </script>
 
