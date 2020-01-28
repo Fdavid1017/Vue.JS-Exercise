847f517d1656b0c2a47b5dcd50439db7fa4fac58
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-01-28 01:00:04 +0100

    ShowIt: added wikipedia link

--- show_it/src/components/GalaxyInfos.vue
+++ show_it/src/components/GalaxyInfos.vue
@@ -40,6 +40,12 @@
                 {{this.data!==null?((this.data.meaning===null||this.data.meaning==='')?'':this.data.meaning):''}}
             </div>
         </div>
+        <div class="row w-100 justify-content-center">
+            <a class="w-100 mr-2 ml-2" v-bind:href="this.wikiUrl" target="_blank">
+                {{this.data!==null?((this.data.name===null||this.data.name==='')?'':
+                'Click here to see more information about the '+this.data.name+' on the Wikipedia!'):''}}
+            </a>
+        </div>
     </div>
 </template>
 
@@ -53,9 +59,23 @@
             'data'
         ],
         name: "GalaxyInfos",
+
+        computed: {
+            wikiUrl: function () {
+                if (this.data === null || this.data.name === null || this.data.name === '') {
+                    return '';
+                }
+
+                return 'https://en.wikipedia.org/wiki/' + this.data.name;
+            }
+        },
     }
 </script>
 
 <style scoped>
-
+    a {
+        text-decoration: none;
+        color: #f5f5f5 !important;
+        text-align: center !important;
+    }
 </style>
