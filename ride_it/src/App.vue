d74099dec98d179937a559055f21bea051cab3f1
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-01-30 17:54:36 +0100

    RideIt: added navbar + assets, removing base file (Helloworld.vue, etc)

--- ride_it/src/App.vue
+++ ride_it/src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="nav" class="d-inline-block">
+      <div class="logo">
+        <router-link to="/">
+          <img src="@/assets/carLogoWhite.svg" alt="logo"
+        /></router-link>
+      </div>
+      <div><router-link to="/Login">Login</router-link></div>
+      <div><router-link to="/Register">Register</router-link></div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+<!--
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+-->
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -22,7 +32,7 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #0080ff;
 
     &.router-link-exact-active {
       color: #42b983;
