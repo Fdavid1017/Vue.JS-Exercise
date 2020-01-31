471d56cc4f672014ab61786fb25147b3ac952edc
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-02-01 00:44:51 +0100

    RideIt: login view created

--- ride_it/src/views/Login.vue
+++ ride_it/src/views/Login.vue
@@ -1,5 +1,109 @@
 <template>
-  <div>
-    Login
+  <div id="loginBlock" class="w-100 justify-content-center">
+    <div id="formContainer" class="panel row m-5">
+      <div class="col">
+        <div id="welcomeText" class="row mt-3 justify-content-center">
+          Welcome
+        </div>
+        <div class="row mt-3 justify-content-center">
+          <img id="logo" src="@/assets/carLogoBlack.svg" alt="Logo" />
+        </div>
+        <div class="row mt-3">
+          <MyInput
+            type="email"
+            name="Email"
+            v-bind:hasError="errors.includes('emailError')"
+            v-model="user.firstName"
+          />
+        </div>
+        <div class="row mt-3">
+          <MyInput
+            type="password"
+            name="Password"
+            v-bind:hasError="errors.includes('passwordError')"
+            v-model="user.firstName"
+          />
+        </div>
+        <div class="row mt-3 justify-content-center">
+          <a v-on:click="register()" class="myButton">
+            Login
+          </a>
+        </div>
+        <div class="row mt-3 justify-content-center register">
+          Dont have an account yet?
+        </div>
+        <router-link class="row justify-content-center register" to="/Register"
+          >Register</router-link
+        >
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+/* eslint-disable space-before-function-paren */
+
+import MyInput from '../components/MyInput.vue'
+export default {
+  components: {
+    MyInput
+  },
+  name: 'Login',
+  data: function() {
+    return {
+      user: {
+        password: '',
+        email: ''
+      },
+      errors: []
+    }
+  },
+  methods: {
+    register: function() {
+      // this.errors = register(this.$store, this.account)
+      console.log(this.errors)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/variables.scss';
+
+.myButton {
+  font-weight: bold;
+}
+
+.register {
+  color: $lightTextColor;
+  font-size: 15px;
+  line-height: 21px;
+  align-items: center;
+  text-align: center;
+
+  a {
+    text-decoration: underline !important;
+    color: $lightTextColor !important;
+  }
+}
+
+#welcomeText {
+  color: $textColor;
+  font-weight: bold;
+  font-size: 2em;
+}
+
+#logo {
+  height: 150px;
+}
+
+#loginBlock {
+  background-image: url('../assets/city.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  display: flex;
+  align-items: center;
+}
+</style>
