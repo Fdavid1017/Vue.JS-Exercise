1a35c33767823a409921daa99632fd85777dc3af
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-02-08 15:44:53 +0100

    RideIt: created base spring app

---
+++ ride_it_Spring/src/test/java/com/ride_it/ride_it/RideItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ride_it.ride_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
