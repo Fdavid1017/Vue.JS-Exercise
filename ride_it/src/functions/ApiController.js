28dcdccbe43185c911c51a563f18d3281a20ce07
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-02-11 09:26:06 +0100

    RideItSpring: CORS error

--- ride_it/src/functions/ApiController.js
+++ ride_it/src/functions/ApiController.js
@@ -4,5 +4,5 @@ import axios from 'axios'
 const BASE_URL = 'localhost:8081/'
 
 export function AddUser(user) {
-  axios.post(BASE_URL + 'user/create', user)
+  axios.post(BASE_URL + 'user/create', user).catch(e => console.log(e))
 }
--- ride_it_Spring/src/main/java/fdavid/ride_it_spring/RideItSpringApplication.java
+++ ride_it_Spring/src/main/java/fdavid/ride_it_spring/RideItSpringApplication.java
@@ -9,5 +9,4 @@ public class RideItSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RideItSpringApplication.class, args);
 	}
-
 }
