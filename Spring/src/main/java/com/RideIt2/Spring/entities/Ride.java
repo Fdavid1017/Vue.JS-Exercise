90ec64e9f2466d908aaa065dc100e3e226e83ecc
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-02-26 13:12:32 +0100

    Created improved spring boot server

---
+++ Spring/src/main/java/com/RideIt2/Spring/entities/Ride.java
@@ -0,0 +1,39 @@
+package com.RideIt2.Spring.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ride {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "driver_email")
+    private User driver;
+    private String fromLocation;
+    private String toLocation;
+    private LocalDateTime whenTime;
+    private int spaces;
+    private String car;
+    private String description;
+    @OneToMany(cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
+    @JoinColumn(name = "passenger_email")
+    private List<User> passengers;
+}
