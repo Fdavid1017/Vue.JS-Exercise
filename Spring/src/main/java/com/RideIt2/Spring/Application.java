4cd5c21e0d485a4616cbe852d2cf4a3cb48bf8ff
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-02-27 11:33:12 +0100

    Spring implemented swagger

--- Spring/src/main/java/com/RideIt2/Spring/Application.java
+++ Spring/src/main/java/com/RideIt2/Spring/Application.java
@@ -10,7 +10,13 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
@@ -27,5 +33,11 @@ public class Application {
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.RideIt2.Spring")).build();
 	}
 }
